7d3e3cf5e098861610d00dd0bfd5b4021fdd6ba7
Tim Paul <contact@timpaul.co.uk>
2026-03-06 23:28:36 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/doom-loop/sw.js', { scope: '/doom-loop/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
